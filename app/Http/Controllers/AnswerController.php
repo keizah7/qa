f60d53c89b25854bda356184652475910622a35a
keizah7 <murashka.arturas@gmail.com>
2019-04-04 17:53:39 +0300

    Update answer

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -31,10 +31,8 @@ class AnswerController extends Controller
     public function edit(Question $question, Answer $answer)
     {
         $this->authorize('update', $answer);
-
         return view('answers.edit', compact('question', 'answer'));
     }
-
     /**
      * Update the specified resource in storage.
      *
@@ -45,22 +43,21 @@ class AnswerController extends Controller
     public function update(Request $request, Question $question, Answer $answer)
     {
         $this->authorize('update', $answer);
-
         $answer->update($request->validate([
             'body' => 'required',
         ]));
-
         return redirect()->route('questions.show', $question->slug)->with('success', 'Your answer has been updated');
     }
-
     /**
      * Remove the specified resource from storage.
      *
      * @param  \App\Answer  $answer
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Answer $answer)
+    public function destroy(Question $question, Answer $answer)
     {
-        //
+        $this->authorize('delete', $answer);
+        $answer->delete();
+        return back()->with('success', "Your answer has been removed");
     }
 }
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -15,6 +15,7 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         // 'App\Model' => 'App\Policies\ModelPolicy',
         'App\Question' => 'App\Policies\QuestionPolicy',
+        'App\Answer' => 'App\Policies\AnswerPolicy',
     ];
 
     /**
