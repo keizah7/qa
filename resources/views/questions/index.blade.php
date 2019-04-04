db5f0d0acd007703447425612be27ac6a78d5ab6
keizah7 <murashka.arturas@gmail.com>
2019-04-04 10:02:14 +0300

    Question delete

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -12,10 +12,12 @@
                             <a href="{{ route('questions.create') }}" class="btn btn-outline-secondary">Ask Question</a>
                         </div>
                     </div>
+
                 </div>
 
                 <div class="card-body">
                     @include ('layouts._messages')
+
                     @foreach ($questions as $question)
                         <div class="media">
                             <div class="d-flex flex-column counters">
@@ -26,11 +28,21 @@
                                     <strong>{{ $question->answers }}</strong> {{ str_plural('answer', $question->answers) }}
                                 </div>
                                 <div class="view">
-                                    {{ $question->views .' '. str_plural('view', $question->views) }}
+                                    {{ $question->views . " " . str_plural('view', $question->views) }}
                                 </div>
                             </div>
                             <div class="media-body">
-                                <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
+                                <div class="d-flex align-items-center">
+                                    <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
+                                    <div class="ml-auto">
+                                        <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
+                                        <form class="form-delete" method="post" action="{{ route('questions.destroy', $question->id) }}">
+                                            @method('DELETE')
+                                            @csrf
+                                            <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
+                                        </form>
+                                    </div>
+                                </div>
                                 <p class="lead">
                                     Asked by
                                     <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
@@ -42,7 +54,7 @@
                         <hr>
                     @endforeach
 
-                    <div class="">
+                    <div class="mx-auto">
                         {{ $questions->links() }}
                     </div>
                 </div>
